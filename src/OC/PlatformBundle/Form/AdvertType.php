fc937121fbb81fcce6cdbbe0e32909324572d61c
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2017-11-29 10:48:05 +0100

    add expanded option in advert type form

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -33,6 +33,7 @@ class AdvertType extends AbstractType
         'class'         => 'OCPlatformBundle:Category',
         'choice_label'  => 'name',
         'multiple'      => true,
+        'expanded'      => true,
         'query_builder' => function(CategoryRepository $repository) use($pattern) {
           return $repository->getLikeQueryBuilder($pattern);
         }
